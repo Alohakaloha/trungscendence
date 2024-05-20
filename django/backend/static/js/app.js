e98b42ad1d8ad24b30a579aa8c9c4d3ec90bcf2c
Trung <dnguyen@student.42wolfsburg.de>
2024-05-20 13:07:35 +0200

    broken chat window

--- django/backend/static/js/app.js
+++ django/backend/static/js/app.js
@@ -1,5 +1,7 @@
 // on refresh handle the routing
 const content = document.getElementById('content');
+const chat = document.getElementById('chat');
+
 let jsFile;
 
 window.onpopstate = function(event) {
@@ -9,6 +11,7 @@ window.onpopstate = function(event) {
 window.onload = function() {
 	handleRouting();
 };
+
 
 async function fetchUserData(){
 	try {
@@ -67,6 +70,7 @@ async function handleRouting() {
 		
 		if (user.authenticated){
 			document.getElementById('profile_picture').src = user.profile_picture;
+			chatSocket = new WebSocket('wss://' + window.location.host + '/ws/chatting/');
 		}
 		switch (page) {
 			case '/':
@@ -75,7 +79,6 @@ async function handleRouting() {
 				break;
 
 			case '/chat':
-				jsFile = './chat.js';
 				showPage(`${page.slice(1)}/${page.slice(1)}.html`);
 				break;
 
@@ -174,7 +177,6 @@ async function currentJS() {
 		case '/profile':
 			break;
 			case '/chat':
-				unloadEvents('./chat.js');
 				break;
 				case '/history':
 					break;
@@ -230,6 +232,57 @@ async function currentJS() {
 		}
 	});
 	
+//        | |         | |  
+//     ___| |__   __ _| |_ 
+//    / __| '_ \ / _` | __|
+//   | (__| | | | (_| | |_ 
+//    \___|_| |_|\__,_|\__|
+	let chatSocket;
+
+	chat.addEventListener('click', async function(){
+		let user = await fetchUserData();
+		chat.style.height = 'auto';
+		chat.style.width = 'auto';
+		if (user.authenticated){
+			if (!chatSocket){
+				console.log("something went wrong")
+				return;
+			}
+			chat.innerHTML = '<div><div id="chat-window"></div><div id="chat-input"><input type="text" id="chat-message" ><button class="btn btn-dark" id="chat-button">Send</button></div></div>';
+
+
+			chatSocket.onopen = function(){
+				console.log("Socket is open");
+			};
+
+			chatSocket.onmessage = function(event) {
+					console.log(`Data received from server: ${event.data}`);
+					let chatWindow = document.getElementById('chat-window');
+					chatWindow.innerText += event.data + "\n";
+					chatWindow.scrollTop = chatWindow.scrollHeight;
+			};
+
+			chatSocket.onclose = function(event){
+				if (event.code === 1000) {
+					console.log(`Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+				} else {
+					console.log('Connection died');
+				}
+			}
+		}
+		else{
+			chat.innerHTML = 'Please log in to use chat';
+		}
+	});
+
+	chat.addEventListener('mouseleave', function(){
+		chat.style.height = '3vh';
+		chat.style.width = '5vw';
+		chat.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16"><path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/></svg>';
+	});
+
+
+
 	
 	async function callSettings(path) {
 		return await fetch("game/"+path+".html")
