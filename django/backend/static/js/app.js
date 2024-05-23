ca2e90a04a999f0e2de1277a17780fbbfc0cc728
Trung <dnguyen@student.42wolfsburg.de>
2024-05-23 13:56:27 +0200

    chat socket fixed on logout

--- django/backend/static/js/app.js
+++ django/backend/static/js/app.js
@@ -93,9 +93,18 @@ async function handleRouting() {
 		
 		if (user.authenticated){
 			document.getElementById('profile_picture').src = user.profile_picture;
-			chatSocket = new WebSocket('wss://' + window.location.host + '/ws/chatting/');
+			if (chatSocket){
+			}else{
+				chatSocket = new WebSocket('wss://' + window.location.host + '/ws/chatting/');
+			}	
 			chatSocket.onopen = function(){
 				console.log("Socket is open");
+			}
+		}
+		else{
+			if (chatSocket){
+				chatSocket.close();
+				chatSocket = null;
 			}
 		}
 		switch (page) {
@@ -317,11 +326,15 @@ async function currentJS() {
 
 
 	function openingChat(){
+		if (!chatSocket)
+		{
+			alert("Please log in to use chat");
+			return;
+		}
 		console.log("opening chat")
 		openWindow = true;
 		chat.style.height = 'auto';
 		chat.style.width = 'auto';
-		chat.style.display = 'block';
 		let closing = document.createElement("div");
 		closing.id = 'close-chat';
 		chat.innerHTML = '<div id="chatWindow-Wrapper"><div id="chat-window"><div id="chat-text"></div></div></div><div id="chat-input"><input type="text" id="chat-message" ><button class="btn btn-dark" id="chat-button" onclick="sendChat()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16"><path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/></svg></button></div></div>';
@@ -333,6 +346,7 @@ async function currentJS() {
 			event.stopPropagation();
 			closingChat();
 		});
+		chat.style.transform = 'translate(0, 0)';
 		friendList = document.createElement('div');
 		friendList.id = 'friend-list';
 		showFriends();
@@ -404,9 +418,9 @@ async function currentJS() {
 		chatWindowWrapper = null;
 		chat.style.height = '3vh';
 		chat.style.width = '5vw';
-		chat.style.display = 'flex';
 		chat.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16"><path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/></svg>';
 		chat.addEventListener('click', openingChat);
+		chat.style.transform = 'translate(0, -100%)';
 	};
 
 
