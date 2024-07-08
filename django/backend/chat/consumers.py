78ab76775edf0733f19ed1cdb563d17d2523e755
Maximilian Wagner <mwagner@students.42wolfsburg.de>
2024-07-08 23:36:15 +0200

    small consumer refactor

--- django/backend/chat/consumers.py
+++ django/backend/chat/consumers.py
@@ -5,8 +5,6 @@ from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 DEFAULT_ROOM_NAME = 'chatting'
-
-# Dictionary to map usernames to their WebSocket channels
 user_channel_mapping = {}
 
 def logprint(*args, **kwargs):
@@ -14,16 +12,11 @@ def logprint(*args, **kwargs):
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        from .models import Chat, Message
-        from auth_app.models import AppUser
-
         self.room_name = DEFAULT_ROOM_NAME
         self.room_group_name = f'chat_{self.room_name}'
         self.username = self.scope['user'].username
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
-        # Add user to user_channel_mapping
         user_channel_mapping[self.username] = self.channel_name
 
         await self.channel_layer.group_send(
@@ -39,9 +32,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        from .models import Chat, Message
-        from auth_app.models import AppUser
-
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -53,14 +43,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-
-        # Remove user from user_channel_mapping
         user_channel_mapping.pop(self.username, None)
 
     async def receive(self, text_data):
-        from .models import Chat, Message
-        from auth_app.models import AppUser
-
         try:
             chat_json = json.loads(text_data)
             action_type = chat_json.get('type')
@@ -83,33 +68,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         except (KeyError, json.JSONDecodeError) as e:
             logprint(f"Invalid JSON: {text_data}, Error: {e}")
-        except AppUser.DoesNotExist as e:
-            logprint(f"User '{receiver_username}' does not exist: {e}")
         except Exception as e:
             logprint(f"An error occurred: {e}")
 
     async def handle_message(self, sender_username, receiver_username, message_content):
-        from .models import Chat, Message
-        from auth_app.models import AppUser
-
-        sender = await sync_to_async(AppUser.objects.get)(username=sender_username)
-
         if receiver_username == 'global':
-            await self.broadcast_message(message_content, sender.username)
+            await self.broadcast_message(message_content, sender_username)
         else:
-            await self.send_private_message(sender, receiver_username, message_content)
+            await self.send_private_message(sender_username, receiver_username, message_content)
 
     async def broadcast_message(self, message_content, sender_username):
-        from .models import Block
-        from auth_app.models import AppUser
-
-        sender = await sync_to_async(AppUser.objects.get)(username=sender_username)
-
-        # Fetch all users who have blocked the sender asynchronously
-        blocked_users = await sync_to_async(lambda: list(Block.objects.filter(blocked=sender).values_list('blocker__username', flat=True)))()
-        blocked_users = set(blocked_users)
-
-        # Loop through all connected users
+        blocked_users = await self.get_blocked_users(sender_username)
+        
         for username, channel in user_channel_mapping.items():
             if username not in blocked_users:
                 await self.channel_layer.send(
@@ -123,31 +93,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     }
                 )
 
-    async def send_private_message(self, sender, receiver_username, message_content):
+    async def send_private_message(self, sender_username, receiver_username, message_content):
         from .models import Chat, Message, Block
         from auth_app.models import AppUser
 
+        sender = await sync_to_async(AppUser.objects.get)(username=sender_username)
         receiver = await sync_to_async(AppUser.objects.get)(username=receiver_username)
         blocked = await sync_to_async(Block.objects.filter(blocker=receiver, blocked=sender).exists)()
 
         if blocked:
             logprint(f"Message from {sender.username} to {receiver.username} is blocked and will not be delivered.")
-            
             sender_channel = user_channel_mapping.get(sender.username)
-            # Inform the sender that they have been blocked
             if sender_channel:
-                system_message_event = {
+                await self.channel_layer.send(sender_channel, {
                     'type': 'chat_message',
                     'message': f"You have been blocked by {receiver_username} and your message was not delivered.",
                     'sender': 'system',
                     'timestamp': self.get_current_timestamp(),
-                }
-                await self.channel_layer.send(sender_channel, system_message_event)
-            
+                })
             return
 
         receiver_channel = user_channel_mapping.get(receiver_username)
-
         if receiver_channel:
             chat = await sync_to_async(Chat().find_or_create_chat)(sender, receiver)
             await sync_to_async(Message.objects.create)(
@@ -170,15 +136,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 await self.channel_layer.send(sender_channel, message_event)
         else:
             logprint(f"Receiver '{receiver_username}' is not connected")
-            system_message_event = {
-                'type': 'chat_message',
-                'message': f"{receiver_username} is not connected",
-                'sender': 'system',
-                'timestamp': self.get_current_timestamp(),
-            }
             sender_channel = user_channel_mapping.get(sender.username)
             if sender_channel:
-                await self.channel_layer.send(sender_channel, system_message_event)
+                await self.channel_layer.send(sender_channel, {
+                    'type': 'chat_message',
+                    'message': f"{receiver_username} is not connected",
+                    'sender': 'system',
+                    'timestamp': self.get_current_timestamp(),
+                })
 
     async def block_user(self, chat_json):
         from auth_app.models import AppUser
@@ -238,13 +203,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
             await self.send(text_data=json.dumps({
                 'type': 'message',
-                'message': f"{receiver.username} was not blocked",
+                'message': f"{receiver_username} was not blocked",
                 'sender': 'system',
                 'timestamp': self.get_current_timestamp(),
             }))
 
     async def handle_chatroom(self, sender_username, receiver_username):
-        from .models import Chat, Message
+        from .models import Chat
         from auth_app.models import AppUser
 
         sender = await sync_to_async(AppUser.objects.get)(username=sender_username)
@@ -254,9 +219,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps(chat_messages))
 
     async def chat_message(self, event):
-        from .models import Chat, Message
-        from auth_app.models import AppUser
-
         message = event['message']
         sender = event['sender']
         timestamp = event['timestamp']
@@ -274,3 +236,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         current_time = timezone.now()
         local_time = timezone.localtime(current_time)
         return local_time.strftime('%d.%m.%Y %H:%M')
+
+    @sync_to_async
+    def get_blocked_users(self, username):
+        from .models import Block
+        from auth_app.models import AppUser
+
+        user = AppUser.objects.get(username=username)
+        blocked_users = Block.objects.filter(blocked=user).values_list('blocker__username', flat=True)
+        return set(blocked_users)
