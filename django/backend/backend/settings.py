3f4a922896d492bc5b349be7761f364391063639
Vladimir Angirov <angirov@posteo.net>
2024-07-24 20:58:05 +0200

    make monitoring great again

--- django/backend/backend/settings.py
+++ django/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'nginx', 'django']
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
