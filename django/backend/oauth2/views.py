c4e4562340b1fa1afd19b8e2e38d3169856404ab
Vladimir Angirov <vangirov@students.42wolfsburg.de>
2024-08-10 16:19:08 +0200

    seems to work

--- django/backend/oauth2/views.py
+++ django/backend/oauth2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import authenticate, login as auth_login, views as auth_views
-import urllib.parse, json, os
+import json, os
 import http.client
 from auth_app.views import register_view
 
@@ -16,38 +16,40 @@ BASE_URL = "https://api.intra.42.fr/oauth/authorize"
 USERDATA_ENDPOINT = "https://api.intra.42.fr/v2/me"
 OAUTH_PASSWORD_LENGTH=16
 
-redirect_url = f"{BASE_URL}?client_id={CLIENT_ID}&redirect_uri={urllib.parse.quote_plus(REDIRECT_URI)}&response_type=code"
+redirect_url = f"{BASE_URL}?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code"
 # Create your views here.
 def oauth_login(request):
     return redirect(redirect_url)
 
 def oauth_redirect(request):
     code = request.GET.get("code")
-    data = {
+    data = json.dumps({
         "grant_type": "authorization_code",
         "client_id": CLIENT_ID,
         "client_secret" : CLIENT_SECRET,
         "redirect_uri": REDIRECT_URI,
         "code": code
-    }
+    })
     headers = {
         'Content-Type': 'application/json'
     }
     try:
+        # return HttpResponse(data)
         conn = http.client.HTTPSConnection('api.intra.42.fr')
         conn.request('POST', '/oauth/token', data, headers)
         response_raw = conn.getresponse()
-
         if response_raw.status == 200:
             response = json.loads(response_raw.read().decode('utf-8'))  
             access_token = response.get("access_token")
-
-            token_headers = {"Authorization": f"Bearer {access_token}"}
-            user_data_req = urllib.request.Request(USERDATA_ENDPOINT, headers=token_headers)
-            with urllib.request.urlopen(user_data_req) as response:
-                user_json = response.read()
-            user_data = json.loads(user_json)
-
+            user_headers = {
+                "Authorization": f"Bearer {access_token}",
+                "Content-type": "application/json"
+            }
+            conn = http.client.HTTPSConnection('api.intra.42.fr')
+            conn.request('GET', '/v2/me', headers=user_headers)
+            data_response_raw = conn.getresponse()
+            if data_response_raw.status == 200:
+                user_data = json.loads(data_response_raw.read().decode('utf-8'))
             user = authenticate(request, email=user_data["email"])
             if user is not None:
                 auth_login(request, user, backend='oauth2.auth_backend.PasswordlessAuthBackend')
@@ -61,7 +63,6 @@ def oauth_redirect(request):
                     pic_url=user_data['image']['versions']['small']
                 )
                 auth_login(request, user, backend='oauth2.auth_backend.PasswordlessAuthBackend')
-                # return JsonResponse({'status':'success: reg, login'})
                 return redirect("/profile")
         else:
             return HttpResponse({'status':'success', 'message':'Redirected. failed to get user data'})
